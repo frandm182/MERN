da6c34d0c157c31fc688cbafb140ab495f903104
Francisco Javier Diaz Martin <fjdiaz@stratio.com>
2018-10-03 14:53:43 +0200

    Add init template

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => `
+    <!doctype html>
+    <html>
+        <head>
+            <meta charset="utf8">
+            <title>MERN</title>
+        </head>
+        <body>
+            <div id="root"></div>
+            <script type="text/javascript" src="/dist/bundle.js"></script>
+        </body>
+    </html>
+`;
